283237f98cfbb17e1830cb4498e19bb00b1e9678
Oscar Robinson <oscarrobinson@btinternet.com>
2014-04-07 19:15:29 +0100

    account authentication implemented

--- makesensepy.py
+++ makesensepy.py
@@ -1,7 +1,14 @@
 import requests
 
 #REQUEST CODES
-addSensorCode = "1"
+authenticationCode = "1"
+addSensorCode = "10"
+
+class AuthenticationException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 class MakeSenseDBException(Exception):
     def __init__(self, value):
@@ -9,11 +16,28 @@ class MakeSenseDBException(Exception):
     def __str__(self):
         return repr(self.value)
 
-def addSensor(sensorId, netId, sensorOnt, sensorName="", sensorDescription=""):
-	print sensorName+sensorDescription
-	payload = {'requestCode': addSensorCode, 'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt, 'sensorDescription':sensorDescription, 'sensorName':sensorName}
-	r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
-	if r.text == "23000":
-		raise MakeSenseDBException("A sensor with Id '"+sensorId+"' already exists in the database")
+def getConnection(username, apiId):
+	return MakeSenseConnection(username, apiId)
 
-addSensor("thing","thing2","12")
+
+class MakeSenseConnection:
+	def __init__(self, username, apiId):
+		payload = {'requestCode':authenticationCode, 'username':username, 'apiId':apiId}
+		r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
+		if r.text=="1001":
+			raise AuthenticationException("username or API access id is not valid")
+		else:
+			self.username = username
+			self.id = apiId
+
+	def addSensor(self,sensorId, netId, sensorOnt, sensorName="", sensorDescription=""):
+		print sensorName+sensorDescription
+		payload = {'requestCode': addSensorCode, 'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt, 'sensorDescription':sensorDescription, 'sensorName':sensorName, 'username': self.username, 'id':self.id}
+		r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
+		if r.text == "23000":
+			raise MakeSenseDBException("A sensor with Id '"+sensorId+"' already exists in the database")
+
+
+conn = getConnection("oscar",11)
+conn.addSensor("lolId", "anNetwork", "1")
+#addSensor("thing","thing2","12")
