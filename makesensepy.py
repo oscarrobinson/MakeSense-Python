dc6540b0789b6cd31b7062bb9809608ed386625a
Oscar Robinson <oscarrobinson@btinternet.com>
2014-04-07 17:19:54 +0100

    added initial error handling for addSensor

--- makesensepy.py
+++ makesensepy.py
@@ -1,7 +1,19 @@
 import requests
 
-def addSensor(sensorId, netId, sensorOnt):
-	payload = {'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt}
-	r = requests.post("http://uclteam10.azurewebsites.net/testpost.php", data=payload)
+#REQUEST CODES
+addSensorCode = "1"
+
+class MakeSenseDBException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def addSensor(sensorId, netId, sensorOnt, sensorName="", sensorDescription=""):
+	print sensorName+sensorDescription
+	payload = {'requestCode': addSensorCode, 'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt, 'sensorDescription':sensorDescription, 'sensorName':sensorName}
+	r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
+	if r.text == "23000":
+		raise MakeSenseDBException("A sensor with Id '"+sensorId+"' already exists in the database")
 
 addSensor("thing","thing2","12")
