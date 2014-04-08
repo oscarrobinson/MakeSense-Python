f618631eec2dbe4425133760fbc79ef40c4eba27
Oscar Robinson <oscarrobinson@btinternet.com>
2014-04-08 18:55:24 +0100

    added ability to add readings, fixed some stuff, improved error reporting

--- makesensepy.py
+++ makesensepy.py
@@ -1,8 +1,11 @@
 import requests
 
+address = "http://localhost:8888/apicontroller.php"
+
 #REQUEST CODES
 authenticationCode = "1"
 addSensorCode = "10"
+addReadingCode  = "20"
 
 class AuthenticationException(Exception):
     def __init__(self, value):
@@ -16,6 +19,13 @@ class MakeSenseDBException(Exception):
     def __str__(self):
         return repr(self.value)
 
+class DataException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
 def getConnection(username, apiId):
 	return MakeSenseConnection(username, apiId)
 
@@ -23,7 +33,7 @@ def getConnection(username, apiId):
 class MakeSenseConnection:
 	def __init__(self, username, apiId):
 		payload = {'requestCode':authenticationCode, 'username':username, 'apiId':apiId}
-		r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
+		r = requests.post(address, data=payload)
 		if r.text=="1001":
 			raise AuthenticationException("username or API access id is not valid")
 		else:
@@ -31,13 +41,42 @@ class MakeSenseConnection:
 			self.id = apiId
 
 	def addSensor(self,sensorId, netId, sensorOnt, sensorName="", sensorDescription=""):
-		print sensorName+sensorDescription
+		if len(sensorId)>30:
+			raise DataException("SensorId is too long (Max 30 chars)")
+		if len(netId)>20:
+			raise DataException("NetworkId is too long (Max 20 chars)")
+		try:
+			int(sensorOnt)
+		except ValueError:
+			raise DataException("Sensor Ontology Code is not an integer")
+		if len(sensorName)>150:
+			raise DataException("SensorName is too long (Max 150 chars)")
+		if len(sensorDescription)>20000:
+			raise DataException("SensorDescription is too long (Max 20000 chars)")
 		payload = {'requestCode': addSensorCode, 'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt, 'sensorDescription':sensorDescription, 'sensorName':sensorName, 'username': self.username, 'id':self.id}
-		r = requests.post("http://uclteam10.azurewebsites.net/apicontroller.php", data=payload)
+		r = requests.post(address, data=payload)
 		if r.text == "23000":
 			raise MakeSenseDBException("A sensor with Id '"+sensorId+"' already exists in the database")
+		elif r.text[:4] == "1001":
+			raise AuthenticationException("username or API access id is not valid")
+
+	def addReading(self, sensorId, reading, timestamp):
+		if len(sensorId)>30:
+			raise DataException("SensorId is too long (Max 30 chars)")
+		if len(reading)>40:
+			raise DataException("Reading is too long (Max 40 chars)")
+		try:
+			int(timestamp)
+		except ValueError:
+			raise DataException("Timestamp is not a valid int")
+		payload = {'requestCode':addReadingCode, 'username': self.username, 'id':self.id, 'sensorId': sensorId, 'reading':reading, 'timestamp':timestamp}
+		r = requests.post(address, data=payload)
+		if r.text[:4] == "1001":
+			raise AuthenticationException("username or API access id is not valid")
+		elif r.text == "23000":
+			raise MakeSenseDBException("Data at time "+timestamp+" already exists for sensor with id "+sensorId)
 
 
 conn = getConnection("oscar",11)
-conn.addSensor("lolId", "anNetwork", "1")
+conn.addReading("robkera","112", "121341");
 #addSensor("thing","thing2","12")
